fe55e950642da509a6c23d1ced01e84ccdb85edb
Dario Radecic <radecicdario@gmail.com>
2020-05-03 14:16:08 +0200

    Initial Commit

---
+++ DogCat_Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogCat_Application
+//
+//  Created by Dario Radecic on 03/05/2020.
+//  Copyright © 2020 Dario Radecic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
